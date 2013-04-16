34cd502a83c523c14a376146e79b54f009553987
runarberg <runarberg@zoho.com>
2013-04-16 09:36:59 +0000

    Cleaned the news_admin code a little bit

--- protected/pkg/news_admin/urls.py
+++ protected/pkg/news_admin/urls.py
@@ -2,10 +2,11 @@ import views
 
 URLS = [
     (r'^/?$', views.index),
-    (r'^login/?$', views.login),
-    (r'^logout/?$', views.logout),
     (r'^posts/?$', views.posts),
     (r'^new_post/?$', views.new_post),
     (r'^posts/(\d+)/?$', views.post_detail),
     (r'^(\d+)/edit/?$', views.edit_post),
+    (r'^reset/?$', views.reset_db),
+    (r'^login/?$', views.login),
+    (r'^logout/?$', views.logout)
 ]
--- protected/pkg/news_admin/views.py
+++ protected/pkg/news_admin/views.py
@@ -1,21 +1,25 @@
-import os, sys, cgi, Cookie
+import os
+import sys
+import cgi
+from datetime import datetime
 sys.path.append("/home/protected/pkg")
 sys.path.append("/home/sterna/Verkefni/Hellvar webpage/protected/pkg")
 
 from jinja2 import Environment, FileSystemLoader
-from datetime import datetime
 from markdown import markdown
 
 from wsgi_app import security
 from news import models
-news = modles.News()
 
 URLARG = "news_admin.urlargs"
+news = models.News()
 
 # Template support
+
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = Environment(loader=FileSystemLoader(template_dir),
                         autoescape=True)
+
 def format_datetime(timestr):
     time = datetime.strptime(timestr, "%Y-%m-%d %H:%M:%S.%f")
     return time.strftime("%d. %b %Y, %H:%M")
@@ -26,7 +30,8 @@ def markdown_filter(text):
 jinja_env.filters['datetime'] = format_datetime
 jinja_env.filters['markdown'] = markdown_filter
 
-# handy helper functions
+# Handy helper functions
+
 def render(template, *args, **kwargs):
     template = jinja_env.get_template(template)
     return [str(template.render(*args, **kwargs)), ]
@@ -43,61 +48,23 @@ def request_method(environ):
 def RequestForm(environ):
     return cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ)
 
+def get_urlargs(environ, urlarg):
+    return environ[urlarg]
+
+# The Views
+
 # /admin
 def index(environ, start_response):    
     if request_method(environ) == "GET":
+        posts = news.get(items=['id', 'title'])
         start_200(start_response)
-
-    elif request_method(environ) == "POST":
-        news.reset()
-        news.save()
-            
-        redirect(start_response, '/posts')
-
     return render('index.html', **locals())
-    
-# /admin/login/
-def login(environ, start_response):
-    if request_method(environ) == "GET":
-        start_200(start_response)
-
-    elif request_method(environ) == "POST":
-        form = RequestForm(environ)
-        user_id = form.getvalue('user_id')
-        password = form.getvalue('password')
-        authorized_users = [user.username for user in security.USERS]
-
-        unhandled = True
-        if user_id in authorized_users:
-            # first get the correct user object
-            user = filter(lambda x: x.username == user_id, security.USERS)[0]
-            
-            if password == user.password:
-                # make user hash
-                user_hash = security.make_secure_val(user_id)
-                start_response("301 Redirect",
-                               [("Set-Cookie",
-                                 "user_id={0}; Path=/".format(user_hash)),
-                                ("Location", '/')])
-                unhandled = False
-                
-        if unhandled:
-            auth_error = True
-            start_200(start_response)
-    
-    return render('login.html', **locals())
-
-# /admin/logout/
-def logout(environ, start_response):
-    start_response("200 OK",
-                   [("Set-Cookie", "user_id=; Path=/"),
-                    ("Content-Type", 'text/html')])
-    return render('logout.html', **locals())
 
 # /admin/posts/
 def posts(environ, start_response):
+    posts = news.get(items=['id', 'title', 'published', 'last_modified'])
+    
     if request_method(environ) == "GET":
-        posts = news.get(items=['id', 'title', 'published', 'last_modified'])
         start_200(start_response)
         
     elif request_method(environ) == "POST":
@@ -105,12 +72,54 @@ def posts(environ, start_response):
         post_id = form.getvalue('post_id')
         action = form.getvalue('action')
         
-        if post_id and (action == "delete"):
-            news.delete(where={'id': post_id})
-            news.save()
+        if post_id:
+            if action == "delete":
+                if isinstance(post_id, list):
+                    for id in post_id:
+                        news.delete(where={'id': id})
+                else:
+                    news.delete(where={'id': post_id})
+
+                news.save()
+                start_response('301 Redirect', [('Content-Type', 'text/html')])
+                return render('redirect.html', href='posts')     
+
+            elif action == "edit":
+                if isinstance(post_id, str):
+                    redirect(start_response, "{0}/edit".format(post_id))
+                else:
+                    select_to_many_error = True
+                    start_200(start_response)
+
+            elif action == "publish":
+                time = datetime.now()
+                if isinstance(post_id, list):
+                    for id in post_id:
+                        news.update(items={'published': time},
+                                    where={'id': id})
+                else:
+                    news.update(items={'published': time},
+                                where={'id': post_id})
+                news.save()
+                start_response('301 Redirect', [('Content-Type', 'text/html')])
+                return render('redirect.html', href='posts')
+
+            elif action == "unpublish":
+                if isinstance(post_id, list):
+                    for id in post_id:
+                        news.update(items={'published': ''},
+                                    where={'id': id})
+                else:
+                    news.update(items={'published': ''},
+                                where={'id': post_id})
+                news.save()
+                start_response('301 Redirect', [('Content-Type', 'text/html')])
+                return render('redirect.html', href='posts')     
+
+        else:
+            select_none_error = True
+            start_200(start_response)
             
-        redirect(start_response, '/posts')
-        
     return render('posts.html', **locals())
     
 # /admin/post/'post_id'/
@@ -183,3 +192,55 @@ def edit_post(environ, start_response):
         redirect(start_response, "/posts/{0}".format(post_id))
         
     return render('edit_post.html', **locals())
+
+# /admin/reset/
+def reset_db(environ, start_response):
+    if request_method(environ) == "GET":
+        start_200(start_response)
+        
+    elif request_method(environ) == "POST":
+        news.reset()
+        news.save()
+            
+        redirect(start_response, '/')
+
+    return render('reset.html', **locals())
+    
+# /admin/login/
+def login(environ, start_response):
+    if request_method(environ) == "GET":
+        start_200(start_response)
+
+    elif request_method(environ) == "POST":
+        form = RequestForm(environ)
+        user_id = form.getvalue('user_id')
+        password = form.getvalue('password')
+        authorized_users = [user.username for user in security.USERS]
+
+        unhandled = True
+        if user_id in authorized_users:
+            # first get the correct user object
+            user = filter(lambda x: x.username == user_id, security.USERS)[0]
+            
+            if password == user.password:
+                # make user hash
+                user_hash = security.make_secure_val(user_id)
+                start_response("301 Redirect",
+                               [("Set-Cookie",
+                                 "user_id={0}; Path=/".format(user_hash)),
+                                ("Location", '/')])
+                unhandled = False
+                
+        if unhandled:
+            auth_error = True
+            start_200(start_response)
+    
+    return render('login.html', **locals())
+
+# /admin/logout/
+def logout(environ, start_response):
+    start_response("301 Redirect",
+                   [("Set-Cookie", "user_id=; Path=/"),
+                    ("Content-Type", 'text/html')])
+    return render('redirect.html', href="/")
+
